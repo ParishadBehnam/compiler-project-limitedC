2b96120d9274bbf6fa81aba6fbfd174b953446c1
afra amini <afra.amini@gmail.com>
2017-06-05 12:02:32 +0430

    first commit

---
+++ Parser/src/Parser.java
@@ -0,0 +1,20 @@
+/**
+ * Created by afra on 6/5/17.
+ */
+import java.util.*;
+public class Parser {
+}
+class ParseTable {
+    ArrayList<HashMap <String, String>> actionTable;
+    ArrayList<HashMap<String, String>> gotoTable;
+
+    public ParseTable() {
+        actionTable = new ArrayList<>();
+        gotoTable = new ArrayList<>();
+
+        HashMap<String, String> row0 = new HashMap<>();
+
+        actionTable.add(new HashMap<>());
+    }
+
+}
